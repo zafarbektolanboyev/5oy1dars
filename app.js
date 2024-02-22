4d93fc1d2dc516f44bd158aeec0083d2d71f28c5
zafarbek tolanboyev <zafarvektolanboyev618@gmail.com>
2024-02-22 20:02:51 +0500

    htmljs

---
+++ app.js
@@ -0,0 +1,21 @@
+// 
+// syntaxis xato
+// let a = 1
+// const a = 1
+
+//  Run time xato
+// console.log(a);
+// let a = 1;
+
+// let b = 2;
+// console.log(b);
+
+// let c = 3;
+// console.log(c); 
+
+// try{
+//     console.log(a);
+//     let a = 1;
+// }catch{
+//     console.log("Xatolik yuz berdi")
+// }
